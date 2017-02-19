9c87ade06ef3f7ff04d53b35ba0039150e31b554
rawqing <kingstar95d@gmail.com>
2017-02-19 22:29:50 +0800

    Initial commit

---
+++ grails-app/domain/racetrack/Race.groovy
@@ -0,0 +1,18 @@
+package racetrack
+
+class Race {
+
+    String name
+    Date startDate
+    String city
+    String state
+    BigDecimal distance
+    BigDecimal cost
+    Integer maxRunners = 100000
+    static constraints = {
+    }
+
+    static mapping = {
+        sort "startDate"
+    }
+}
